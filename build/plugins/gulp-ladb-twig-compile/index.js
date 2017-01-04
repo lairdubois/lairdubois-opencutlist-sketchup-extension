7e72dc3124870ed27c00476f1e6c559754c6368e
mobilarte <martin@maxsyma.com>
2017-01-03 21:31:29 -0700

    Fix for compilation on Windows
    Fixing gulp script with respect to path separator and end-of-line

--- build/plugins/gulp-ladb-twig-compile/index.js
+++ build/plugins/gulp-ladb-twig-compile/index.js
@@ -2,6 +2,7 @@ var through = require('through2');
 var gutil = require('gulp-util');
 var twig = require('twig').twig;
 var merge = require('merge');
+var slash = require('slash');
 
 var PluginError = gutil.PluginError;
 
@@ -18,11 +19,13 @@ module.exports = function (opt) {
         }, opt);
         var data;
         try {
-            var template = twig({id: file.relative, data: file.contents.toString('utf8')});
+            var template = twig({id: slash(file.relative), data: file.contents.toString('utf8')});
             data = template.compile(options);
 
             // LADB Sanitize
             data = data.replace('twig({', 'Twig.twig({allowInlineIncludes:true, ');
+            //Fix for compiling on Windows (including slash above)
+            data = data.replace(/\\r\\n/g, '\\n');
 
         } catch (err) {
             return cb(new PluginError('gulp-ladb-twig-compile', err));
