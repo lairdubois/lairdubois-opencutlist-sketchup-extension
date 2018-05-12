db94f8ab5d427c38088f0a8df223b4f42ed056d9
bbeaulant <bo@borisbeaulant.com>
2018-05-12 08:31:34 +0200

    + Build DEV RBZ

--- src/ladb_opencutlist/ruby/plugin.rb
+++ src/ladb_opencutlist/ruby/plugin.rb
@@ -13,7 +13,7 @@ module Ladb::OpenCutList
 
     NAME = 'OpenCutList'.freeze
     VERSION = '1.5.0-dev'.freeze
-    BUILD = '201804291338'.freeze
+    BUILD = '201805120630'.freeze
 
     DEFAULT_SECTION = ATTRIBUTE_DICTIONARY = 'ladb_opencutlist'.freeze
     BC_DEFAULT_SECTION = BC_ATTRIBUTE_DICTIONARY = 'ladb_toolbox'.freeze
