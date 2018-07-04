cb8d09ad437f8a3a29b2083d83e491e30f165414
bbeaulant <bo@borisbeaulant.com>
2018-07-04 11:05:34 +0200

    ~ Cutting diagram UI improvemnts + 1.5.1-dev

--- src/ladb_opencutlist/ruby/constants.rb
+++ src/ladb_opencutlist/ruby/constants.rb
@@ -2,6 +2,6 @@ module Ladb::OpenCutList
 
     EXTENSION_NAME = 'OpenCutList'.freeze
     EXTENSION_VERSION = '1.5.1-dev'.freeze
-    EXTENSION_BUILD = '201806281330'.freeze
+    EXTENSION_BUILD = '201807040905'.freeze
 
 end
