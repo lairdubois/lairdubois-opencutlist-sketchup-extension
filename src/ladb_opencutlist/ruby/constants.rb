aeb356b41859ee1c9d3b91949a19ee25f682c38a
bbeaulant <bo@borisbeaulant.com>
2019-02-10 15:40:22 +0100

    + Cutlist labels filter autocomplete -dev

--- src/ladb_opencutlist/ruby/constants.rb
+++ src/ladb_opencutlist/ruby/constants.rb
@@ -2,6 +2,6 @@ module Ladb::OpenCutList
 
     EXTENSION_NAME = 'OpenCutList'.freeze
     EXTENSION_VERSION = '1.6.0-dev'.freeze
-    EXTENSION_BUILD = '201902101424'.freeze
+    EXTENSION_BUILD = '201902101440'.freeze
 
 end
