e183834cfa7e592edd926d458f4684310c7397ed
bbeaulant <bo@borisbeaulant.com>
2019-05-06 10:07:32 +0200

    ~ Fixed cutlist crash on Sketchup 2014

--- src/ladb_opencutlist/ruby/constants.rb
+++ src/ladb_opencutlist/ruby/constants.rb
@@ -2,6 +2,6 @@ module Ladb::OpenCutList
 
     EXTENSION_NAME = 'OpenCutList'.freeze
     EXTENSION_VERSION = '1.6.3-dev'.freeze
-    EXTENSION_BUILD = '201905011538'.freeze
+    EXTENSION_BUILD = '201905060807'.freeze
 
 end
