e5b5a0f27ca29656d23bf629dc9ed58b459511ca
bbeaulant <bo@borisbeaulant.com>
2019-05-09 10:57:50 +0200

    ~ rm unused cutlist group menu item

--- src/ladb_opencutlist/ruby/constants.rb
+++ src/ladb_opencutlist/ruby/constants.rb
@@ -2,6 +2,6 @@ module Ladb::OpenCutList
 
     EXTENSION_NAME = 'OpenCutList'.freeze
     EXTENSION_VERSION = '1.6.3-dev'.freeze
-    EXTENSION_BUILD = '201905060807'.freeze
+    EXTENSION_BUILD = '201905090708'.freeze
 
 end
