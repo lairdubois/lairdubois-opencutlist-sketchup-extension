adae1bd05f229b4c385ddda14b90f54cdf4d2542
bbeaulant <bo@borisbeaulant.com>
2019-05-23 11:53:11 +0200

    ~ Real area computation fix

--- src/ladb_opencutlist/ruby/constants.rb
+++ src/ladb_opencutlist/ruby/constants.rb
@@ -2,6 +2,6 @@ module Ladb::OpenCutList
 
     EXTENSION_NAME = 'OpenCutList'.freeze
     EXTENSION_VERSION = '1.6.4-dev'.freeze
-    EXTENSION_BUILD = '201905222124'.freeze
+    EXTENSION_BUILD = '201905230952'.freeze
 
 end
