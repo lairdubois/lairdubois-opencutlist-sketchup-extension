f73a19193dd2e7ca5d67475741ab4be813bfce9d
bbeaulant <bo@borisbeaulant.com>
2019-05-27 05:48:20 +0200

    + 1.7.0-dev (i18n updates)

--- src/ladb_opencutlist/ruby/constants.rb
+++ src/ladb_opencutlist/ruby/constants.rb
@@ -2,6 +2,6 @@ module Ladb::OpenCutList
 
     EXTENSION_NAME = 'OpenCutList'.freeze
     EXTENSION_VERSION = '1.7.0-dev'.freeze
-    EXTENSION_BUILD = '201905261335'.freeze
+    EXTENSION_BUILD = '201905270347'.freeze
 
 end
