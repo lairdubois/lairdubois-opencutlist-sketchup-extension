f23bdd38663532f6a9c4f58e743221b735d517c6
bbeaulant <bo@borisbeaulant.com>
2019-06-04 16:19:46 +0200

    ~ Dimensions help improvements

--- src/ladb_opencutlist/ruby/constants.rb
+++ src/ladb_opencutlist/ruby/constants.rb
@@ -2,6 +2,6 @@ module Ladb::OpenCutList
 
     EXTENSION_NAME = 'OpenCutList'.freeze
     EXTENSION_VERSION = '1.7.0-dev'.freeze
-    EXTENSION_BUILD = '201906041128'.freeze
+    EXTENSION_BUILD = '201906041419'.freeze
 
 end
