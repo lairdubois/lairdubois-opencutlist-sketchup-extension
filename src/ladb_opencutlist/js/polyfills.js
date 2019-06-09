6bbcc49b6b1d50c10d14c2816e86e31f275e27d0
bbeaulant <bo@borisbeaulant.com>
2019-06-09 12:42:09 +0200

    + Added context menu to edit selected part properties from model selection

--- src/ladb_opencutlist/js/polyfills.js
+++ src/ladb_opencutlist/js/polyfills.js
@@ -23,3 +23,38 @@ if (!String.prototype.endsWith) {
         return lastIndex !== -1 && lastIndex === position;
     };
 }
+
+// https://tc39.github.io/ecma262/#sec-array.prototype.includes
+if (!Array.prototype.includes) {
+    Object.defineProperty(Array.prototype, 'includes', {
+        value: function(searchElement, fromIndex) {
+
+            if (this == null) {
+                throw new TypeError('"this" est nul ou non défini');
+            }
+
+            var o = Object(this);
+            var len = o.length >>> 0;
+
+            if (len === 0) {
+                return false;
+            }
+
+            var n = fromIndex | 0;
+            var k = Math.max(n >= 0 ? n : len - Math.abs(n), 0);
+
+            function sameValueZero(x, y) {
+                return x === y || (typeof x === 'number' && typeof y === 'number' && isNaN(x) && isNaN(y));
+            }
+
+            while (k < len) {
+                if (sameValueZero(o[k], searchElement)) {
+                    return true;
+                }
+                k++;
+            }
+
+            return false;
+        }
+    });
+}
