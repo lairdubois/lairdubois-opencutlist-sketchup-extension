627cb097f625e89d5c8975a253e391906b98e422
bbeaulant <bo@borisbeaulant.com>
2019-07-10 16:29:55 +0200

    ~ IE fix

--- src/ladb_opencutlist/ruby/constants.rb
+++ src/ladb_opencutlist/ruby/constants.rb
@@ -2,6 +2,6 @@ module Ladb::OpenCutList
 
     EXTENSION_NAME = 'OpenCutList'.freeze
     EXTENSION_VERSION = '1.7.2-dev'.freeze
-    EXTENSION_BUILD = '201907101324'.freeze
+    EXTENSION_BUILD = '201907101429'.freeze
 
 end
