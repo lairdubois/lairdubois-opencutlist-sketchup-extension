7e7755e99b954398df0fbbfa572530c85f8c2c7b
bbeaulant <bo@borisbeaulant.com>
2019-09-09 18:45:10 +0200

    + Edge (grouping fix)

--- src/ladb_opencutlist/ruby/constants.rb
+++ src/ladb_opencutlist/ruby/constants.rb
@@ -2,6 +2,6 @@ module Ladb::OpenCutList
 
     EXTENSION_NAME = 'OpenCutList'.freeze
     EXTENSION_VERSION = '1.8.0-dev'.freeze
-    EXTENSION_BUILD = '201909091603'.freeze
+    EXTENSION_BUILD = '201909091644'.freeze
 
 end
