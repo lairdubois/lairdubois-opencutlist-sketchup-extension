10a2bceeb8e5574145ee12749af4386e79496382
bbeaulant <bo@borisbeaulant.com>
2019-10-28 08:03:20 +0100

    ~ Bug fix https://github.com/lairdubois/lairdubois-opencutlist-sketchup-extension/issues/110

--- src/ladb_opencutlist/ruby/constants.rb
+++ src/ladb_opencutlist/ruby/constants.rb
@@ -2,6 +2,6 @@ module Ladb::OpenCutList
 
     EXTENSION_NAME = 'OpenCutList'.freeze
     EXTENSION_VERSION = '1.8.1-dev'.freeze
-    EXTENSION_BUILD = '201910271821'.freeze
+    EXTENSION_BUILD = '201910280702'.freeze
 
 end
