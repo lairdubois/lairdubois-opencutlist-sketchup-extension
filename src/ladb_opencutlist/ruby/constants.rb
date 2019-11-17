6e971fb4fd7ca820cb611d82c82f444dbb3ed1d6
bbeaulant <bo@borisbeaulant.com>
2019-11-17 19:14:53 +0100

    + Sponsor tab to master

--- src/ladb_opencutlist/ruby/constants.rb
+++ src/ladb_opencutlist/ruby/constants.rb
@@ -2,6 +2,6 @@ module Ladb::OpenCutList
 
     EXTENSION_NAME = 'OpenCutList'.freeze
     EXTENSION_VERSION = '1.8.1-dev'.freeze
-    EXTENSION_BUILD = '201910312049'.freeze
+    EXTENSION_BUILD = '201911171814'.freeze
 
 end
