a7e59f765062639103899cc6afd89b2c256e77c9
bbeaulant <bo@borisbeaulant.com>
2020-04-23 08:16:04 +0200

    ~ Master merge + 1.9.x

--- src/ladb_opencutlist/ruby/constants.rb
+++ src/ladb_opencutlist/ruby/constants.rb
@@ -1,7 +1,7 @@
 module Ladb::OpenCutList
 
     EXTENSION_NAME = 'OpenCutList'.freeze
-    EXTENSION_VERSION = '1.8.4'.freeze
-    EXTENSION_BUILD = '202001091041'.freeze
+    EXTENSION_VERSION = '1.9.0-dev'.freeze
+    EXTENSION_BUILD = '202004230614'.freeze
 
 end
