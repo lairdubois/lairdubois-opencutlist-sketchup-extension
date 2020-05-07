ad2f843b22d289d81fd01021e593e3b31eebce7d
mobilarte <martin@maxsyma.com>
2020-05-07 19:04:42 +0200

    ~ Fixing/Breaking Stuff in 1D

--- src/ladb_opencutlist/ruby/lib/bin_packing_1d/bin.rb
+++ src/ladb_opencutlist/ruby/lib/bin_packing_1d/bin.rb
@@ -1,7 +1,7 @@
 module Ladb::OpenCutList::BinPacking1D
   class Bin < Packing1D
-    attr_accessor :type, :length, :x,  :boxes, :trimsize, :saw_kerf,
-                  :current_leftover, :efficiency, :current_position,
+    attr_accessor :type, :x, :length, :boxes, :trimsize, :saw_kerf,
+                  :efficiency, :current_position, :current_leftover,
                   :cuts, :total_length_cuts
                       
     def initialize(length, type, options = nil)
@@ -14,42 +14,61 @@ module Ladb::OpenCutList::BinPacking1D
       @boxes = []                        # placed boxes
       @cuts = []
       @current_leftover = @length - @trimsize - @saw_kerf
-      @current_position = @trimsize + @saw_kerf
-      @cuts << @current_position if @current_position > 0 # first cut
-      
+      if @trimsize > 0
+        @cuts << @trimsize
+        @current_position = @trimsize
+      else
+        @current_position = @trimsize + @saw_kerf
+      end
       @net_length_parts = 0
       @efficiency = 3.1415
-      @total_length_cuts = 0
+      @total_length_cuts = 0             # does not really make sense here!
     end
 
     def add (box)
+      # add a box to this bin and update position and leftover
+      dbg("   adding box #{box.length} after #{@current_position}")
+      if @current_position + box.length > @length
+        dbg("BIG BIG PROBLEM")
+        exit
+      end
       @boxes << box
-      @current_leftover = @current_leftover - box.length - @saw_kerf
+      # if start is left
+      #   . first cut is on the left sider of this mark (waste side)
+      #   . next cuts are on the right side of this mark (waste side)
+      @cuts << @current_position + box.length
       @current_position += box.length + @saw_kerf
-      @cuts << @current_position if @current_position < @length
+      @current_leftover = @length - @trimsize - @current_position
+      dbg("   new #{@current_position}")
       @net_length_parts += box.length
       @efficiency = (@length - @current_leftover)/@length.to_f
     end
     
     def print()
+      # debugging only 
       @boxes.each do |b|
         dbg("   box length = #{b.length}")
       end
     end
     
-    def netlength
-      return @length - 2 * @options.trimsize
+    def netlength()
+      # return the net (available) length of this bin
+      # cannot be smaller than 0
+      return [@length - 2 * @options.trimsize, 0].max
     end
     
     def leftover
+      # MUST be removed, access over instance variable
       @current_leftover
     end
     
     def nb_of_cuts
+      # MUST be removed, access over instance variable
       return @cuts.length
     end
 
     def all_lengths
+      # funky code, REVIEW!
       if @type == BAR_TYPE_UNFIT
         @length
       else
@@ -69,9 +88,5 @@ module Ladb::OpenCutList::BinPacking1D
       end
     end
     
-    #def compute_efficiency
-    #  @efficiency = (@length - @current_leftover)/@length.to_f
-    #end
-
   end
 end
--- src/ladb_opencutlist/ruby/lib/bin_packing_1d/packengine.rb
+++ src/ladb_opencutlist/ruby/lib/bin_packing_1d/packengine.rb
@@ -20,7 +20,7 @@ module Ladb::OpenCutList::BinPacking1D
     
     # add scrap bars to be used
     def add_bin(length)
-      dbg("adding BIN length #{length}")
+      dbg("   . BIN length #{length}")
       newbin = Bin.new(length, BIN_TYPE_LO, @options)
       # sorting is not necessary, algorithm will pick largest available
       @leftovers.push(newbin).sort_by!(&:length)
@@ -28,20 +28,20 @@ module Ladb::OpenCutList::BinPacking1D
         
     # add boxes (for the 1D case, we will call these "parts")
     def add_box(length, data = nil)
-      dbg("adding BOX length=#{length}, data=#{data}")
+      dbg("   . BOX length=#{length}, data=#{data}")
       @boxes << Box.new(length, data)
     end
     
     # run the packing, if successful, run also basic stats
     def run
-      dbg("-> packengine run")
+      dbg("-- packengine run")
       return nil, ERROR_NO_PARTS if @boxes.empty?
       return nil, ERROR_NO_BIN if @options.base_bin_length < EPS && @leftovers.empty?
       
       @warnings << WARNING_SAW_KERF_SMALL if @options.saw_kerf < EPS
       @warnings << WARNING_TRIM_SIZE_LARGE if @options.trimsize > MAX_TRIMSIZE_FACTOR*@options.saw_kerf
       
-      dbg("-> create packer nb leftovers=#{@leftovers.length}") 
+      dbg("-> create packer with nb of leftovers=#{@leftovers.length}") 
       packer = Packer.new(@options)
       packer.leftovers = @leftovers
       packer.boxes = @boxes
@@ -49,11 +49,7 @@ module Ladb::OpenCutList::BinPacking1D
       dbg("-> running packer")
       err = packer.run()
       
-      if err == ERROR_NONE or err == ERROR_SUBOPT 
-        packer.prep_results
-      else
-        packer = nil
-      end
+      packer = nil if err > ERROR_SUBOPT
       return packer, err 
     end
   end
