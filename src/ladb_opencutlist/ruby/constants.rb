c137511afe5d4cdbc4a901734036b6d89c52eab0
bbeaulant <bo@borisbeaulant.com>
2020-06-03 07:44:39 +0200

    - Updater verbose + DEV build

--- src/ladb_opencutlist/ruby/constants.rb
+++ src/ladb_opencutlist/ruby/constants.rb
@@ -2,6 +2,6 @@ module Ladb::OpenCutList
 
     EXTENSION_NAME = 'OpenCutList'.freeze
     EXTENSION_VERSION = '1.9.0-dev'.freeze
-    EXTENSION_BUILD = '202006011533'.freeze
+    EXTENSION_BUILD = '202006030543'.freeze
 
 end
