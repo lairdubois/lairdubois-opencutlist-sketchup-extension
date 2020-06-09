2bf3daa79e6efd658fa24a77c68e165d19fa9d80
bbeaulant <bo@borisbeaulant.com>
2020-06-09 09:20:14 +0200

    + std dimensions tooltips

--- src/ladb_opencutlist/ruby/constants.rb
+++ src/ladb_opencutlist/ruby/constants.rb
@@ -1,7 +1,7 @@
 module Ladb::OpenCutList
 
     EXTENSION_NAME = 'OpenCutList'.freeze
-    EXTENSION_VERSION = '1.9.1-dev-dev'.freeze
-    EXTENSION_BUILD = '202006080637'.freeze
+    EXTENSION_VERSION = '1.9.1-dev'.freeze
+    EXTENSION_BUILD = '202006090647'.freeze
 
 end
