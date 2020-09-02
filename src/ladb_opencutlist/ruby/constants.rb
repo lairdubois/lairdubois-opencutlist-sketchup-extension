20208bca2a95b0892f947bade17a40ec123be95c
bbeaulant <bo@borisbeaulant.com>
2020-09-02 13:49:38 +0200

    ~ Fixed cutting diagrams 2D rotated part dimensions display bug

--- src/ladb_opencutlist/ruby/constants.rb
+++ src/ladb_opencutlist/ruby/constants.rb
@@ -1,7 +1,7 @@
 module Ladb::OpenCutList
 
     EXTENSION_NAME = 'OpenCutList'.freeze
-    EXTENSION_VERSION = '1.9.3'.freeze
-    EXTENSION_BUILD = '202008271931'.freeze
+    EXTENSION_VERSION = '1.9.4-dev'.freeze
+    EXTENSION_BUILD = '202009021148'.freeze
 
 end
