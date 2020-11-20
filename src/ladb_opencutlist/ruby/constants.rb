150b27344fc4554cf5bf437cb2622f42b20631de
bbeaulant <bo@borisbeaulant.com>
2020-11-20 11:27:18 +0100

    ~ Labels to Tags fix

--- src/ladb_opencutlist/ruby/constants.rb
+++ src/ladb_opencutlist/ruby/constants.rb
@@ -1,7 +1,7 @@
 module Ladb::OpenCutList
 
     EXTENSION_NAME = 'OpenCutList'.freeze
-    EXTENSION_VERSION = '1.9.9'.freeze
-    EXTENSION_BUILD = '202011181225'.freeze
+    EXTENSION_VERSION = '1.9.10-dev'.freeze
+    EXTENSION_BUILD = '202011201020'.freeze
 
 end
