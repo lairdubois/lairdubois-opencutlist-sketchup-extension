70724231970c380cb3345fa78dbc23b325109613
bbeaulant <bo@borisbeaulant.com>
2020-12-12 12:20:04 +0100

    + 1.9.10 pre-submit

--- src/ladb_opencutlist/ruby/constants.rb
+++ src/ladb_opencutlist/ruby/constants.rb
@@ -1,7 +1,7 @@
 module Ladb::OpenCutList
 
     EXTENSION_NAME = 'OpenCutList'.freeze
-    EXTENSION_VERSION = '1.9.10-dev'.freeze
-    EXTENSION_BUILD = '202012040641'.freeze
+    EXTENSION_VERSION = '1.9.10'.freeze
+    EXTENSION_BUILD = '202012091141'.freeze
 
 end
