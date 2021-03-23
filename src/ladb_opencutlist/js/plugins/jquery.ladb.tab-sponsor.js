9775fb4163c2169210dd19c772fb8a41dc69db97
bbeaulant <bo@borisbeaulant.com>
2021-03-23 09:28:59 +0100

    ~ Docs objective

--- src/ladb_opencutlist/js/plugins/jquery.ladb.tab-sponsor.js
+++ src/ladb_opencutlist/js/plugins/jquery.ladb.tab-sponsor.js
@@ -23,7 +23,7 @@
     LadbTabSponsor.prototype.bindObjectiveWidget = function ($widget) {
         var that = this;
 
-        var objectiveGoal = 5000;
+        var objectiveGoal = 3500;
         var objectiveCurrency = 'USD';
 
         // Fetch UI elements
