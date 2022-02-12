fa7495c861cdf1e9fa779c5ff1d1b25994d82574
bbeaulant <bo@borisbeaulant.com>
2022-02-12 08:13:38 +0100

    ~ Mini tools print improvement

--- src/ladb_opencutlist/js/constants.js
+++ src/ladb_opencutlist/js/constants.js
@@ -1,7 +1,7 @@
 // CONSTANTS
 // ======================
 
-var EXTENSION_BUILD = '202112120741';   // /!\ Auto-generated line, do not edit //
+var EXTENSION_BUILD = '202202120616';   // /!\ Auto-generated line, do not edit //
 
 // UI /////
 
--- src/ladb_opencutlist/ruby/constants.rb
+++ src/ladb_opencutlist/ruby/constants.rb
@@ -1,7 +1,7 @@
 module Ladb::OpenCutList
 
     EXTENSION_NAME = 'OpenCutList'.freeze
-    EXTENSION_VERSION = '3.0.2-dev'.freeze
-    EXTENSION_BUILD = '202112120741'.freeze
+    EXTENSION_VERSION = '4.0.0-dev'.freeze
+    EXTENSION_BUILD = '202202120616'.freeze
 
 end
