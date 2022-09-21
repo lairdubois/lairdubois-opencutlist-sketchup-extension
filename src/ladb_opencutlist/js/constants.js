48ec87d95c7f00e0ff73f71d779fa8a58fdc8182
bbeaulant <bo@borisbeaulant.com>
2022-09-21 14:19:26 +0200

    + 4.0.0

--- src/ladb_opencutlist/js/constants.js
+++ src/ladb_opencutlist/js/constants.js
@@ -1,7 +1,7 @@
 // CONSTANTS
 // ======================
 
-var EXTENSION_BUILD = '202208310633';   // /!\ Auto-generated line, do not edit //
+var EXTENSION_BUILD = '202209141933';   // /!\ Auto-generated line, do not edit //
 
 // UI /////
 
