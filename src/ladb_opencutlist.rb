ffec54b4d2769bd0f977b828ec7c720e8853c8ab
Boris Beaulant <bo@borisbeaulant.com>
2022-12-21 07:39:17 +0100

    + 4.1.0-dev

--- src/ladb_opencutlist.rb
+++ src/ladb_opencutlist.rb
@@ -43,6 +43,8 @@ module Ladb
         ex.description = 'Planilha de Produção e Gerador de Diagrama de Corte de chapas e pranchas MLC para marceneiros e carpinteiros. Código aberto, crowdfunded e fácil de usar ;)'
       when 'ru'
         ex.description = 'Плагин расчета карт раскроя для деревообработчиков и не только. Открытый исходный код, краундфандинг и простота использования ;)'
+      when 'vi'
+        ex.description = 'Danh sách cắt và Trình tạo sơ đồ cắt cho thợ mộc. Mã nguồn mở, được huy động vốn từ cộng đồng và dễ sử dụng;)'
       when 'zz'
         ex.description = '3 - Cutlist and Cutting Diagram Generator for Woodworkers. Open Source, crowdfunded and easy to use ;)'
       else
