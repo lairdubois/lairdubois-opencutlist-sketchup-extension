f49b9ea1065252c757708c39b6ee2fb1db45e876
Boris Beaulant <bo@borisbeaulant.com>
2023-01-07 09:43:24 +0100

    ~ fixed i18n cutting diagram

--- src/ladb_opencutlist/js/constants.js
+++ src/ladb_opencutlist/js/constants.js
@@ -1,7 +1,7 @@
 // CONSTANTS
 // ======================
 
-var EXTENSION_BUILD = '202301031518';   // /!\ Auto-generated line, do not edit //
+var EXTENSION_BUILD = '202301061948';   // /!\ Auto-generated line, do not edit //
 
 // UI /////
 
--- src/ladb_opencutlist/ruby/constants.rb
+++ src/ladb_opencutlist/ruby/constants.rb
@@ -2,6 +2,6 @@ module Ladb::OpenCutList
 
     EXTENSION_NAME = 'OpenCutList'.freeze
     EXTENSION_VERSION = '5.0.0-dev'.freeze
-    EXTENSION_BUILD = '202301031518'.freeze
+    EXTENSION_BUILD = '202301061948'.freeze
 
 end
