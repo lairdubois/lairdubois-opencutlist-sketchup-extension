6e2fd97483d6c0308617be447a0f1a558958874b
Boris Beaulant <bo@borisbeaulant.com>
2023-01-22 19:49:20 +0100

    + DEV build

--- src/ladb_opencutlist/ruby/constants.rb
+++ src/ladb_opencutlist/ruby/constants.rb
@@ -2,6 +2,6 @@ module Ladb::OpenCutList
 
     EXTENSION_NAME = 'OpenCutList'.freeze
     EXTENSION_VERSION = '5.0.0-dev'.freeze
-    EXTENSION_BUILD = '202301211517'.freeze
+    EXTENSION_BUILD = '202301221849'.freeze
 
 end
