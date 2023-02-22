36aa042e1dabcbebeef93bf2f4fee552bda65ae5
Boris Beaulant <bo@borisbeaulant.com>
2023-02-22 21:27:26 +0100

    ~ Fixed layout button visibility

--- src/ladb_opencutlist/ruby/worker/cutlist/cutlist_generate_worker.rb
+++ src/ladb_opencutlist/ruby/worker/cutlist/cutlist_generate_worker.rb
@@ -705,7 +705,9 @@ module Ladb::OpenCutList
       # Warnings & tips
       if @instance_infos_cache.length > 0
         @material_usages_cache.each { |key, material_usage|
-          if material_usage.type == MaterialAttributes::TYPE_SOLID_WOOD
+          if material_usage.type == MaterialAttributes::TYPE_UNKNOWN
+            cutlist.unknow_material_count += material_usage.use_count
+          elsif material_usage.type == MaterialAttributes::TYPE_SOLID_WOOD
             cutlist.solid_wood_material_count += material_usage.use_count
           elsif material_usage.type == MaterialAttributes::TYPE_SHEET_GOOD
             cutlist.sheet_good_material_count += material_usage.use_count
