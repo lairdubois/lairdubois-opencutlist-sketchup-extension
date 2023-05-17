e9e61fccce0d9b97c7d0b2a25f8134b5785f6b25
Boris Beaulant <bo@borisbeaulant.com>
2023-05-17 11:49:33 +0200

    ~ Improved cutting diagram dimensions display

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,7 @@ in 5.x, 4.x, 3.x, 2.x, 1.x and 0.x versions.
 
 * 5.0.2 (WIP)
 
+  * Improved Smart Tool element picker
   * Improved cutting diagram dimensions display
   
 * 5.0.1 (WIP)
--- src/ladb_opencutlist/ruby/constants.rb
+++ src/ladb_opencutlist/ruby/constants.rb
@@ -1,7 +1,7 @@
 module Ladb::OpenCutList
 
     EXTENSION_NAME = 'OpenCutList'.freeze
-    EXTENSION_VERSION = '5.0.1'.freeze
-    EXTENSION_BUILD = '202305160847'.freeze
+    EXTENSION_VERSION = '5.0.2-dev'.freeze
+    EXTENSION_BUILD = '202305170715'.freeze
 
 end
