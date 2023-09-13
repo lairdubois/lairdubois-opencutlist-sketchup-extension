279382b66fb2c2f70dd72e30b65d2ddbd606b66e
Boris Beaulant <bo@borisbeaulant.com>
2023-09-13 17:47:19 +0200

    ~ Improved AxisUtils

--- src/ladb_opencutlist/ruby/utils/axis_utils.rb
+++ src/ladb_opencutlist/ruby/utils/axis_utils.rb
@@ -3,7 +3,11 @@ module Ladb::OpenCutList
   class AxisUtils
 
     def self.flipped?(x_axis, y_axis, z_axis)
-      !(x_axis * y_axis).samedirection?(z_axis)
+      if skewed?(x_axis, y_axis, z_axis)
+        (x_axis % X_AXIS) * (y_axis % Y_AXIS) * (z_axis % Z_AXIS) < 0
+      else
+        !(x_axis * y_axis).samedirection?(z_axis)
+      end
     end
 
     def self.skewed?(x_axis, y_axis, z_axis)
