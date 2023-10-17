2b7a4323ab2c2e060fcdb096416417d53611bea5
Boris Beaulant <bo@borisbeaulant.com>
2023-10-17 18:16:40 +0200

    + 5.1.0 init

--- src/ladb_opencutlist/js/constants.js
+++ src/ladb_opencutlist/js/constants.js
@@ -1,7 +1,7 @@
 // CONSTANTS
 // ======================
 
-var EXTENSION_BUILD = '202310170744';   // /!\ Auto-generated line, do not edit //
+var EXTENSION_BUILD = '202310171521';   // /!\ Auto-generated line, do not edit //
 
 // UI /////
 
--- src/ladb_opencutlist/ruby/constants.rb
+++ src/ladb_opencutlist/ruby/constants.rb
@@ -1,7 +1,7 @@
 module Ladb::OpenCutList
 
     EXTENSION_NAME = 'OpenCutList'.freeze
-    EXTENSION_VERSION = '5.0.5-dev'.freeze
-    EXTENSION_BUILD = '202310170744'.freeze
+    EXTENSION_VERSION = '5.1.0-dev'.freeze
+    EXTENSION_BUILD = '202310171521'.freeze
 
 end
