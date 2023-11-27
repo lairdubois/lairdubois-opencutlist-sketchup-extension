67e6c265cc6f58019f6104ad2b541c7c335616d3
Boris Beaulant <bo@borisbeaulant.com>
2023-11-27 11:22:19 +0100

    + 6.0.0-dev (202311171344)

--- src/ladb_opencutlist/js/constants.js
+++ src/ladb_opencutlist/js/constants.js
@@ -1,7 +1,7 @@
 // CONSTANTS
 // ======================
 
-var EXTENSION_BUILD = '202311171344';   // /!\ Auto-generated line, do not edit //
+var EXTENSION_BUILD = '202311271021';   // /!\ Auto-generated line, do not edit //
 
 // UI /////
 
