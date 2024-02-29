e138d1a73ee17d96da708f48c58b2029074815ce
Boris Beaulant <bo@borisbeaulant.com>
2024-02-29 10:55:30 +0100

    ~ Improved imagick

--- src/ladb_opencutlist/ruby/utils/image_utils.rb
+++ src/ladb_opencutlist/ruby/utils/image_utils.rb
@@ -32,7 +32,7 @@ module Ladb::OpenCutList
       # Force out_file to be in_file if nil
       out_file = in_file if out_file.nil?
 
-      bin_dir = File.absolute_path(File.join(PLUGIN_DIR,'bin'))
+      bin_dir = File.join(PLUGIN_DIR,'bin')
       case Sketchup.platform
 
         when :platform_osx
