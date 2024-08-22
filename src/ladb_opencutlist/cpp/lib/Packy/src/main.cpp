3f5e03417f89b17a2296bfaeaebb93bde5bebf1b
Boris Beaulant <bo@borisbeaulant.com>
2024-08-22 21:00:37 +0200

    ~ Improved Packy

--- src/ladb_opencutlist/cpp/lib/Packy/src/main.cpp
+++ src/ladb_opencutlist/cpp/lib/Packy/src/main.cpp
@@ -1,7 +1,5 @@
 #include <iostream>
 #include <future>
-#include <thread>
-#include <chrono>
 
 #include "packy.hpp"
 #include "optimizer_builder.hpp"
@@ -9,56 +7,28 @@
 using namespace Packy;
 using namespace nlohmann;
 
-std::shared_future<json> future_;
-
-void optimize_async() {
-  future_ = std::async(std::launch::async, []{
-    json j_ouput;
-
-    try {
-
-      OptimizerBuilder optimizer_builder;
-      Optimizer& optimizer = (*optimizer_builder.build(std::string("input.json")));
-
-      j_ouput = optimizer.optimize();
-
-    } catch(const std::exception& e) {
-      j_ouput["error"] = "\033[1;31mError: " + std::string(e.what()) + "\033[0m";
-    } catch( ... ) {
-      j_ouput["error"] = "\033[1;31mUnknow Error\033[0m";
-    }
-
-    return std::move(j_ouput);
-  }).share();
-}
-
 int main() {
 
   std::cout << "---------------------------------" << std::endl;
   std::cout << "             PACKY" << std::endl;
   std::cout << "---------------------------------" << std::endl;
 
-  optimize_async();
+//  try {
 
-  std::future_status status;
-  while (true) {
+    OptimizerBuilder optimizer_builder;
+    Optimizer& optimizer = (*optimizer_builder.build(std::string("input.json")));
 
-    status = future_.wait_for(std::chrono::milliseconds(0));
-    if (status == std::future_status::ready) {
+    json j_ouput = optimizer.optimize();
 
-      std::cout << "###################################" << std::endl;
-      std::cout << future_.get().dump(1, ' ') << std::endl;
-      std::cout << "###################################" << std::endl;
+    std::cout << "###################################" << std::endl;
+    std::cout << j_ouput.dump(1, ' ') << std::endl;
+    std::cout << "###################################" << std::endl;
 
-      break;
-    }
-
-    std::this_thread::sleep_for(std::chrono::milliseconds(500));
-    std::cout << "." << std::flush;
-
-  }
-
-  std::cout << "Done." << std::endl;
+//  } catch(const std::exception& e) {
+//    std::cout << "\033[1;31mError: " + std::string(e.what()) + "\033[0m" << std::endl;
+//  } catch( ... ) {
+//    std::cout << "\033[1;31mUnknow Error\033[0m" << std::endl;
+//  }
 
   return 0;
 }
--- src/ladb_opencutlist/cpp/lib/Packy/src/packy.cpp
+++ src/ladb_opencutlist/cpp/lib/Packy/src/packy.cpp
@@ -6,9 +6,7 @@
 #include <string>
 #include <stdexcept>
 #include <thread>
-#include <mutex>
 #include <future>
-#include <chrono>
 
 #include <nlohmann/json.hpp>
 
@@ -19,10 +17,8 @@ using namespace Packy;
 extern "C" {
 #endif
 
-std::mutex mtx;
-
-std::shared_future<json> optimize_future_;
-std::string optimize_str_output_;
+static std::shared_future<json> optimize_future_;
+static std::string optimize_str_output_;
 
 DLL_EXPORTS void c_optimize_start(char* input) {
 
@@ -40,9 +36,9 @@ DLL_EXPORTS void c_optimize_start(char* input) {
       j_ouput = optimizer.optimize();
 
     } catch(const std::exception& e) {
-      j_ouput["error"] = "\033[1;31mError: " + std::string(e.what()) + "\033[0m";
+      j_ouput["error"] = e.what();
     } catch( ... ) {
-      j_ouput["error"] = "\033[1;31mUnknow Error\033[0m";
+      j_ouput["error"] = "Unknow Error";
     }
 
     return std::move(j_ouput);
@@ -57,7 +53,10 @@ DLL_EXPORTS char* c_optimize_advance() {
     optimize_str_output_ = optimize_future_.get().dump();
     return (char*)optimize_str_output_.c_str();
   } else {
-    optimize_str_output_ = json{{"running", true}, {"status", status}}.dump();
+    optimize_str_output_ = json{
+      {"running", true},
+      {"status", status}
+    }.dump();
     return (char*)optimize_str_output_.c_str();
   }
 
