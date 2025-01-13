b2ffb6597da6d0db8fc093e8b1170282e7aadf04
Boris Beaulant <bo@borisbeaulant.com>
2025-01-13 15:53:57 +0100

    ~ Updated build version

--- src/ladb_opencutlist/ruby/constants.rb
+++ src/ladb_opencutlist/ruby/constants.rb
@@ -2,7 +2,7 @@ module Ladb::OpenCutList
 
     EXTENSION_NAME = 'OpenCutList'.freeze
     EXTENSION_VERSION = '7.0.0-dev'.freeze
-    EXTENSION_BUILD = '202501111722'.freeze
+    EXTENSION_BUILD = '202501131444'.freeze
 
     DEFAULT_LANGUAGE = 'en'
     ENABLED_LANGUAGES = %w[ar cs de en es fr he it nl pt ru uk zh]
