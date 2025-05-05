863bf0db8b8129b853234a91e49640b4026fc5ce
Boris Beaulant <bo@borisbeaulant.com>
2025-05-05 07:54:21 +0200

    ~ Updated PackingSolver

--- src/ladb_opencutlist/js/constants.js
+++ src/ladb_opencutlist/js/constants.js
@@ -1,7 +1,7 @@
 // CONSTANTS
 // ======================
 
-const EXTENSION_BUILD = '202505030940';   // /!\ Auto-generated line, do not edit //
+const EXTENSION_BUILD = '202505041336';   // /!\ Auto-generated line, do not edit //
 
 // UI /////
 
