cc442a6228a09a5a0646705d3b358ef06a16fec2
Boris Beaulant <bo@borisbeaulant.com>
2025-05-26 10:57:53 +0200

    ~ Improved Kuix label text pixel size

--- src/ladb_opencutlist/ruby/lib/kuix/entity/2d/label.rb
+++ src/ladb_opencutlist/ruby/lib/kuix/entity/2d/label.rb
@@ -8,10 +8,10 @@ module Ladb::OpenCutList::Kuix
       super(id)
       @text = text
       @text_options = {
-          :font => 'Verdana',
-          :size => 15,
-          :bold => false,
-          :align => TextAlignCenter
+        :font => 'Verdana',
+        _text_pixel_size_key => 15,
+        :bold => false,
+        :align => TextAlignCenter
       }
       if Sketchup.version_number >= 2000000000
         @text_options[:vertical_align] = TextVerticalAlignCenter
@@ -35,13 +35,29 @@ module Ladb::OpenCutList::Kuix
       invalidate
     end
 
-    def text_size=(value)
-      size = Sketchup.platform == :platform_win ? value * 0.75 : value  # Windows workaround -> 0.75 = 72 / 96
-      return if @text_options[:size] == size
-      @text_options[:size] = size
+    def _text_pixel_size=(value)
+      return if @text_options[_text_pixel_size_key] == value
+      @text_options[_text_pixel_size_key] = value
       compute_min_size
       compute_letter_width
       invalidate
+    end
+    private :_text_pixel_size=
+
+    def _text_pixel_size
+      @text_options[_text_pixel_size_key]
+    end
+    private :_text_pixel_size
+
+    def _text_pixel_size_key
+      return :pixel_size if Sketchup.version_number >= 2500000000
+      :size
+    end
+    private :_text_pixel_size_key
+
+    def text_size=(value)
+      pixel_size = Sketchup.version_number < 2500000000 && Sketchup.platform == :platform_win ? value * 0.75 : value  # Windows (SU2025-) workaround -> 0.75 = 72 / 96 dpi
+      self._text_pixel_size = pixel_size
     end
 
     def text_bold=(value)
@@ -86,7 +102,7 @@ module Ladb::OpenCutList::Kuix
         @text_point = content_bounds.corner(Bounds2d::CENTER_LEFT)
       end
       if Sketchup.version_number < 2000000000
-        @text_point.y -= @text_options[:size] # Workaround the "center" text to SU prior 2020 where text anchor is top
+        @text_point.y -= _text_pixel_size # Workaround the "center" text to SU prior 2020 where text anchor is top
       end
 
       # Truncate text if necessary
@@ -106,21 +122,21 @@ module Ladb::OpenCutList::Kuix
     end
 
     def compute_letter_width
-      if @text_options[:size]
+      if _text_pixel_size
         if Sketchup.version_number < 2000000000 || Sketchup.active_model.nil?
           # Estimate letter width
-          @letter_width = @text_options[:size].to_i * 0.7
+          @letter_width = _text_pixel_size.to_i * 0.7
         else
-          text_bounds = Sketchup.active_model.active_view.text_bounds(Geom::Point3d.new, 'A', @text_options)
+          text_bounds = Sketchup.active_model.active_view.text_bounds(ORIGIN, 'A', @text_options)
           @letter_width = text_bounds.width
         end
       end
     end
 
     def compute_min_size
-      if @text_options[:size]
+      if _text_pixel_size
         if !Sketchup.active_model.nil? && Sketchup.active_model.active_view.respond_to?(:text_bounds) # SU 2020+
-          text_bounds = Sketchup.active_model.active_view.text_bounds(Geom::Point3d.new, @text, @text_options)
+          text_bounds = Sketchup.active_model.active_view.text_bounds(ORIGIN, @text, @text_options)
           @min_size.set!(
             text_bounds.width,
             text_bounds.height
@@ -128,8 +144,8 @@ module Ladb::OpenCutList::Kuix
         else
           # Estimate text size
           @min_size.set!(
-            @text.length * @text_options[:size].to_i * 0.7,
-            @text_options[:size]
+            @text.length * _text_pixel_size.to_i * 0.7,
+            _text_pixel_size
           )
         end
       end
