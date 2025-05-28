9db9b7fe001b24d586acf5da5604d71f51ddfd8e
Boris Beaulant <bo@borisbeaulant.com>
2025-05-28 15:51:19 +0200

    ~ Improved eval

--- src/ladb_opencutlist/ruby/worker/common/common_eval_formula_worker.rb
+++ src/ladb_opencutlist/ruby/worker/common/common_eval_formula_worker.rb
@@ -1,9 +1,28 @@
 module Ladb::OpenCutList
 
+  require 'Ripper'
   require_relative '../../model/export/export_data'
   require_relative '../../model/export/export_wrapper'
 
   class CommonEvalFormulaWorker
+
+    CONST_BLACK_LIST = [
+      'Kernel',
+      'File',
+      'Sketchup',
+      'Layout',
+      'UI'
+    ]
+
+    IDENT_BLACK_LIST = [
+      'eval',
+      'instance_eval',
+      'class_eval',
+      'module_eval',
+      'exec',
+      'system',
+      'binding'
+    ]
 
     def initialize(
 
@@ -12,7 +31,7 @@ module Ladb::OpenCutList
 
     )
 
-      @formula = formula
+      @formula = formula.to_s
       @data = data
 
     end
@@ -22,7 +41,32 @@ module Ladb::OpenCutList
     def run
       return { :error => 'default.error' } unless @data.is_a?(ExportData)
 
+      puts "Run ---"
+
       begin
+        tokens = Ripper.lex(@formula)
+        tokens.each do |token|
+          pos, type, text, state = token
+
+          puts "#{pos} #{type} #{text} #{state}"
+
+          if type == :on_backtick
+            throw "Forbidden backtick"
+          end
+
+          if type == :on_const
+            throw "Forbidden Const : #{text}" if CONST_BLACK_LIST.include?(text)
+          end
+
+          if type == :on_ident
+            throw "Forbidden Function : #{text}" if IDENT_BLACK_LIST.include?(text)
+          end
+
+          if type == :on_ivar
+            throw "Undefined Variable : #{text}" unless @data.get_binding.receiver.instance_variables.include?(text.to_sym)
+          end
+
+        end
         value = eval(@formula, @data.get_binding)
         value = value.export if value.is_a?(ExportWrapper)
       rescue Exception => e
