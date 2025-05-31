d8af9c7ad30c17f0387a1346feced8c3c9b5dbc9
Boris Beaulant <bo@borisbeaulant.com>
2025-05-31 14:20:21 +0200

    + Added Formula Parser

--- src/ladb_opencutlist/ruby/worker/common/common_eval_formula_worker.rb
+++ src/ladb_opencutlist/ruby/worker/common/common_eval_formula_worker.rb
@@ -7,52 +7,6 @@ module Ladb::OpenCutList
 
   class CommonEvalFormulaWorker
 
-    WHITE_LIST_CONST = {
-      :var_ref => [
-        'Math',
-        'Geom',
-      ]
-    }
-
-    BLACK_LIST_IDENT = {
-      :fcall => [
-        'eval',
-        'exec',
-        'system',
-        'syscall',
-        'exit',
-        'exit!',
-        'binding',
-        'send',
-        'fail',
-      ],
-      :call => [
-        'eval',
-        'instance_eval',
-        'class_eval',
-        'module_eval',
-        'exec',
-        'system',
-        'syscall',
-        'exit',
-        'exit!',
-        'binding',
-        'fail',
-        'send',
-      ],
-      :vcall => [
-        'eval',
-        'exec',
-        'system',
-        'syscall',
-        'exit',
-        'exit!',
-        'binding',
-        'send',
-        'fail',
-      ],
-    }
-
     def initialize(
 
                   formula:,
@@ -88,40 +42,6 @@ module Ladb::OpenCutList
       end
 
       value
-    end
-
-    # -----
-
-    def _check(sexp, prev_symbol = nil)
-      if sexp.is_a?(Array)
-        if sexp[0].is_a?(Symbol)
-          symbol, text = sexp
-          case symbol
-
-          when :xstring_literal
-            # xstring_literal
-            raise "Forbidden Backtick"
-
-          when :@const
-            # var_ref -> @const
-            raise "Forbidden Const : #{text}" unless WHITE_LIST_CONST[prev_symbol].nil? || !WHITE_LIST_CONST[prev_symbol].nil? && WHITE_LIST_CONST[prev_symbol].include?(text)
-
-          when :@ident
-            # fcall | call | vcall -> @ident
-            raise "Forbidden Call : #{text}" if BLACK_LIST_IDENT[prev_symbol] && BLACK_LIST_IDENT[prev_symbol].include?(text)
-
-          when :@ivar
-            # var_ref -> @ivar
-            raise "Undefined variable : #{text}" unless prev_symbol == :var_ref && @data.get_binding.receiver.instance_variables.include?(text.to_sym)
-
-          end
-          prev_symbol = symbol
-          sexp = sexp[1..-1]
-        end
-        sexp.each do |exp|
-          _check(exp, prev_symbol)
-        end
-      end
     end
 
   end
