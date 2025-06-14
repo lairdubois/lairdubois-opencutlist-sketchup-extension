520b90c38fff4ca65e965c348b16440c231b2017
Boris Beaulant <bo@borisbeaulant.com>
2025-06-14 16:33:13 +0200

    ~ Fixed pixel facto for old bin packing

--- src/ladb_opencutlist/ruby/worker/cutlist/cutlist_cuttingdiagram2d_write_worker.rb
+++ src/ladb_opencutlist/ruby/worker/cutlist/cutlist_cuttingdiagram2d_write_worker.rb
@@ -106,6 +106,8 @@ module Ladb::OpenCutList
       return { :errors => [ 'default.error' ] } unless @cuttingdiagram2d && @cuttingdiagram2d.def.group
       return { :errors => [ 'default.error' ] } unless SUPPORTED_FILE_FORMATS.include?(@file_format)
 
+      _set_pixel_to_inch_factor(7)
+
       # Ask for output dir
       dir = UI.select_directory(title: PLUGIN.get_i18n_string('tab.cutlist.cuttingdiagram.write.title'), directory: '')
       if dir
