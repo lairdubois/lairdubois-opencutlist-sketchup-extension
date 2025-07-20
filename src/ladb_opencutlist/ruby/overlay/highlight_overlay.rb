34d9b1078558f6addbde7b501b9eddd197db6f98
Boris Beaulant <bo@borisbeaulant.com>
2025-07-20 09:09:32 +0200

    ~ Improved Highlight Overlay

--- src/ladb_opencutlist/ruby/overlay/highlight_overlay.rb
+++ src/ladb_opencutlist/ruby/overlay/highlight_overlay.rb
@@ -1,7 +1,6 @@
 module Ladb::OpenCutList
 
   require_relative '../lib/kuix/kuix'
-  require_relative '../utils/path_utils'
   require_relative '../utils/color_utils'
 
   class HighlightOverlay < Kuix::KuixOverlay
@@ -19,105 +18,105 @@ module Ladb::OpenCutList
            ignore_edges: true,
            ignore_clines: false
         ).run
-        if @drawing_def
+        if @drawing_def.is_a?(DrawingDef)
 
-        view = Sketchup.active_model.active_view
+          view = Sketchup.active_model.active_view
 
-        # 3D
+          # 3D
 
-        k_group = Kuix::Group.new
-        k_group.transformation = @drawing_def.transformation
-        @space.append(k_group)
+          k_group = Kuix::Group.new
+          k_group.transformation = @drawing_def.transformation
+          @space.append(k_group)
 
-          if @drawing_def.face_manipulators.any?
+            if @drawing_def.face_manipulators.any?
 
-            # Highlight faces
-            k_mesh = Kuix::Mesh.new
-            k_mesh.add_triangles(@drawing_def.face_manipulators.flat_map { |face_manipulator| face_manipulator.triangles })
-            k_mesh.background_color = ColorUtils.color_translucent(highlight_def.color, 80)
-            k_group.append(k_mesh)
+              # Highlight faces
+              k_mesh = Kuix::Mesh.new
+              k_mesh.add_triangles(@drawing_def.face_manipulators.flat_map { |face_manipulator| face_manipulator.triangles })
+              k_mesh.background_color = ColorUtils.color_translucent(highlight_def.color, 80)
+              k_group.append(k_mesh)
+
+            end
+
+            if @drawing_def.cline_manipulators.any?
+
+              # Highlight clines
+              k_segments = Kuix::Segments.new
+              k_segments.add_segments(@drawing_def.cline_manipulators.flat_map { |cline_manipulator| cline_manipulator.segment })
+              k_segments.color = ColorUtils.color_translucent(highlight_def.color)
+              k_segments.line_width = 2
+              k_group.append(k_segments)
+
+            end
+
+            # Box helper
+            k_box = Kuix::BoxMotif.new
+            k_box.bounds.copy!(@drawing_def.bounds)
+            k_box.color = highlight_def.color
+            k_box.line_width = 1
+            k_box.line_stipple = Kuix::LINE_STIPPLE_SHORT_DASHES
+            k_box.on_top = true
+            k_group.append(k_box)
+
+          # 2D
+
+          unless highlight_def.text.nil? || highlight_def.text.empty?
+
+            unit = get_unit(view)
+
+            @canvas.layout = Kuix::StaticLayout.new
+
+            margin = unit * 5
+            min_x = margin
+            min_y = margin
+            max_x = view.vpwidth - margin
+            max_y = view.vpheight - margin
+
+            p = view.screen_coords(@drawing_def.bounds.center.transform(@drawing_def.transformation))
+
+            px = [ [ min_x, p.x.to_i ].max, max_x ].min
+            py = [ [ min_y, p.y.to_i ].max, max_y ].min
+
+            if px == min_x
+              if py == min_y
+                anchor_position = Kuix::Anchor::TOP_LEFT
+              elsif py == max_y
+                anchor_position = Kuix::Anchor::BOTTOM_LEFT
+              else
+                anchor_position = Kuix::Anchor::LEFT
+              end
+            elsif px == max_x
+              if py == min_y
+                anchor_position = Kuix::Anchor::TOP_RIGHT
+              elsif py == max_y
+                anchor_position = Kuix::Anchor::BOTTOM_RIGHT
+              else
+                anchor_position = Kuix::Anchor::RIGHT
+              end
+            else
+              if py == min_y
+                anchor_position = Kuix::Anchor::TOP
+              elsif py == max_y
+                anchor_position = Kuix::Anchor::BOTTOM
+              else
+                anchor_position = Kuix::Anchor::CENTER
+              end
+            end
+
+            k_panel = Kuix::Panel.new
+            k_panel.layout_data = Kuix::StaticLayoutData.new(px, py, -1, -1, Kuix::Anchor.new(anchor_position))
+            k_panel.layout = Kuix::GridLayout.new
+            k_panel.padding.set!(unit, unit, unit * 0.7, unit)
+            k_panel.set_style_attribute(:background_color, highlight_def.color)
+            @canvas.append(k_panel)
+
+              k_lbl = Kuix::Label.new(highlight_def.text)
+              k_lbl.text_size = unit * 3 * get_text_unit_factor
+              k_lbl.text_align = TextAlignLeft
+              k_lbl.set_style_attribute(:color, ColorUtils.color_is_dark?(highlight_def.color) ? Kuix::COLOR_WHITE : Kuix::COLOR_BLACK)
+              k_panel.append(k_lbl)
 
           end
-
-          if @drawing_def.cline_manipulators.any?
-
-            # Highlight clines
-            k_segments = Kuix::Segments.new
-            k_segments.add_segments(@drawing_def.cline_manipulators.flat_map { |cline_manipulator| cline_manipulator.segment })
-            k_segments.color = ColorUtils.color_translucent(highlight_def.color)
-            k_segments.line_width = 2
-            k_group.append(k_segments)
-
-          end
-
-          # Box helper
-          k_box = Kuix::BoxMotif.new
-          k_box.bounds.copy!(@drawing_def.bounds)
-          k_box.color = highlight_def.color
-          k_box.line_width = 1
-          k_box.line_stipple = Kuix::LINE_STIPPLE_SHORT_DASHES
-          k_box.on_top = true
-          k_group.append(k_box)
-
-        # 2D
-
-        unless highlight_def.text.nil? || highlight_def.text.empty?
-
-          unit = get_unit(view)
-
-          @canvas.layout = Kuix::StaticLayout.new
-
-          margin = unit * 5
-          min_x = margin
-          min_y = margin
-          max_x = view.vpwidth - margin
-          max_y = view.vpheight - margin
-
-          p = view.screen_coords(@drawing_def.faces_bounds.center.transform(@drawing_def.transformation))
-
-          px = [ [ min_x, p.x.to_i ].max, max_x ].min
-          py = [ [ min_y, p.y.to_i ].max, max_y ].min
-
-          if px == min_x
-            if py == min_y
-              anchor_position = Kuix::Anchor::TOP_LEFT
-            elsif py == max_y
-              anchor_position = Kuix::Anchor::BOTTOM_LEFT
-            else
-              anchor_position = Kuix::Anchor::LEFT
-            end
-          elsif px == max_x
-            if py == min_y
-              anchor_position = Kuix::Anchor::TOP_RIGHT
-            elsif py == max_y
-              anchor_position = Kuix::Anchor::BOTTOM_RIGHT
-            else
-              anchor_position = Kuix::Anchor::RIGHT
-            end
-          else
-            if py == min_y
-              anchor_position = Kuix::Anchor::TOP
-            elsif py == max_y
-              anchor_position = Kuix::Anchor::BOTTOM
-            else
-              anchor_position = Kuix::Anchor::CENTER
-            end
-          end
-
-          k_panel = Kuix::Panel.new
-          k_panel.layout_data = Kuix::StaticLayoutData.new(px, py, -1, -1, Kuix::Anchor.new(anchor_position))
-          k_panel.layout = Kuix::GridLayout.new
-          k_panel.padding.set!(unit, unit, unit * 0.7, unit)
-          k_panel.set_style_attribute(:background_color, highlight_def.color)
-          @canvas.append(k_panel)
-
-            k_lbl = Kuix::Label.new(highlight_def.text)
-            k_lbl.text_size = unit * 3 * get_text_unit_factor
-            k_lbl.text_align = TextAlignLeft
-            k_lbl.set_style_attribute(:color, ColorUtils.color_is_dark?(highlight_def.color) ? Kuix::COLOR_WHITE : Kuix::COLOR_BLACK)
-            k_panel.append(k_lbl)
-
-        end
 
         end
 
