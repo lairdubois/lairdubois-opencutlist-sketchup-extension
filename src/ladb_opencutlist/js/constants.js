8664a48a89e535f347298dbb0f4bd9252b2b6376
Boris Beaulant <bo@borisbeaulant.com>
2025-10-02 16:37:59 +0200

    + DEV build

--- src/ladb_opencutlist/js/constants.js
+++ src/ladb_opencutlist/js/constants.js
@@ -1,7 +1,7 @@
 // CONSTANTS
 // ======================
 
-const EXTENSION_BUILD = '202510020609';   // /!\ Auto-generated line, do not edit //
+const EXTENSION_BUILD = '202510021436';   // /!\ Auto-generated line, do not edit //
 
 // UI /////
 
--- src/ladb_opencutlist/ruby/constants.rb
+++ src/ladb_opencutlist/ruby/constants.rb
@@ -2,11 +2,10 @@ module Ladb::OpenCutList
 
     EXTENSION_NAME = 'OpenCutList'.freeze
     EXTENSION_VERSION = '7.1.0-dev'.freeze
-    EXTENSION_BUILD = '202510020609'.freeze
+    EXTENSION_BUILD = '202510021436'.freeze
 
     DEFAULT_LANGUAGE = 'en'
-    # ENABLED_LANGUAGES = %w[ar cs de en es fr he hu it nl pl pt ru uk zh]
-    ENABLED_LANGUAGES = %w[ar cs de en es fr hu it pl pt ru sr uk vi zh]
+    ENABLED_LANGUAGES = %w[ar cs de en es fr hu it nl pl pt ru sr uk vi zh]
 
     FILE_FORMAT_SKP = 'skp'.freeze
     FILE_FORMAT_STL = 'stl'.freeze
