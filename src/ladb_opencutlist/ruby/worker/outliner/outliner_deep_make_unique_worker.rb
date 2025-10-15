8ddccec48dc540040953034244ae7119c2d83f4d
Boris Beaulant <bo@borisbeaulant.com>
2025-10-15 17:43:06 +0200

    ~ Fixed outliner deep make unique

--- src/ladb_opencutlist/ruby/worker/outliner/outliner_deep_make_unique_worker.rb
+++ src/ladb_opencutlist/ruby/worker/outliner/outliner_deep_make_unique_worker.rb
@@ -35,26 +35,52 @@ module Ladb::OpenCutList
       node_defs = node_def.get_valid_unlocked_selection_siblings
 
       d_is = {} # Definition => Instances
-      fn_populate_di = lambda { |node_def|
-        next if node_def.entity.deleted?
-        node_def.entity.make_unique if node_def.entity.is_a?(Sketchup::Group) # Force Groups to be unique
-        if node_def.entity.definition.count_instances > 1
-          d_is[node_def.entity.definition] = [] unless d_is.has_key?(node_def.entity.definition)
-          d_is[node_def.entity.definition] << node_def.entity
+      fn_populate_di = lambda { |entity|
+        next if entity.deleted?
+        if entity.is_a?(Sketchup::Group)
+          entity = entity.make_unique # Force Groups to be unique immediately
+        else
+          if entity.definition.count_used_instances > 1
+            d_is[entity.definition] = [] unless d_is.has_key?(entity.definition)
+            d_is[entity.definition] << entity
+          end
         end
-        node_def.children.each do |child_node_def|
-          fn_populate_di.call(child_node_def)
+        if entity.respond_to?(:definition)
+          entity.definition.entities.each do |child_entity|
+            next unless child_entity.respond_to?(:definition)
+            fn_populate_di.call(child_entity)
+          end
         end
       }
-      node_defs.each { |node_def| fn_populate_di.call(node_def) }
+      node_defs.each { |node_def| fn_populate_di.call(node_def.entity) }
 
       d_is.each do |definition, instances|
-        next if instances.size == definition.count_instances   # All instances are there
-        instances.uniq!
+
+        next if instances.size == definition.count_used_instances   # All instances are there, move next
+
+        # Make unique the first instance and retrieve the new definition
         new_definition = instances.shift.make_unique.definition
+
+        # Change definition of all other instances
         instances.each do |instance|
           instance.definition = new_definition
         end
+
+        # Retrieve old definition child instances
+        old_d_cis = definition.entities.to_a
+                  .select { |e| e.is_a?(Sketchup::ComponentInstance) }
+                  .group_by { |i| i.definition }
+
+        # Retrieve new definition child instances
+        new_d_cis = new_definition.entities.to_a
+                  .select { |e| e.is_a?(Sketchup::ComponentInstance) }
+                  .group_by { |i| i.definition }
+
+        # Replace old definition child instances by new definition child instances
+        old_d_cis.each do |d, is|
+          d_is[d].reject! { |i| is.include?(i) }.concat(new_d_cis[d])
+        end
+
       end
 
 
