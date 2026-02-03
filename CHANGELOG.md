64a7367d80c9e22cc2f92f654fc4aa5593ce3290
Boris Beaulant <bo@borisbeaulant.com>
2026-02-03 20:26:18 +0100

    + DEV build

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -14,6 +14,9 @@ in 8.x, 7.x, 6.x, 5.x, 4.x, 3.x, 2.x, 1.x and 0.x versions.
 
 ## Lab
 
+### 2026-02-03
+  * Updated PackingSolver dependency
+  * Fixed part list sorting 
 ### 2026-01-26
   * Updated PackingSolver dependency
 ### 2026-01-25
--- src/ladb_opencutlist/js/constants.js
+++ src/ladb_opencutlist/js/constants.js
@@ -1,7 +1,7 @@
 // CONSTANTS
 // ======================
 
-const EXTENSION_BUILD = '202601260710';   // /!\ Auto-generated line, do not edit //
+const EXTENSION_BUILD = '202602031926';   // /!\ Auto-generated line, do not edit //
 
 // UI /////
 
